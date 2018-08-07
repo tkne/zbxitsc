58dcac3899db3c715d08c46c41dcab773373278f
tomislav <tomislav.knezevic@itsherpa.com>
2018-08-07 17:27:10 +0900

    Updated links within script.

--- Iptables/Shell Script Installers/iptablescheck_install_centos6x.sh
+++ Iptables/Shell Script Installers/iptablescheck_install_centos6x.sh
@@ -2,10 +2,10 @@
 # This script will install iptables status check settings on a Cent OS 6.X server.
 
 # Download iptables config file for Zabbix agent
-wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Configs/iptables.conf -O /etc/zabbix/zabbix_agentd.d/iptables.conf
+wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Configs/iptables.conf -O /etc/zabbix/zabbix_agentd.d/iptables.conf
 
 # Download iptablescheck shell script file for Zabbix agent
-wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Scripts/iptablescheck.sh -O /usr/local/bin/iptablescheck.sh
+wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Scripts/iptablescheck.sh -O /usr/local/bin/iptablescheck.sh
 
 # Make iptablescheck shell script file executable
 chmod +x /usr/local/bin/iptablescheck.sh
