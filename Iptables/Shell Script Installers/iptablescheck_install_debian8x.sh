d5c181b92d102546ab39a30c0420e8fe574d7425
tomislav <tomislav.knezevic@itsherpa.com>
2018-08-08 17:03:43 +0900

    Updated conf target path.

--- Iptables/Shell Script Installers/iptablescheck_install_debian8x.sh
+++ Iptables/Shell Script Installers/iptablescheck_install_debian8x.sh
@@ -2,7 +2,7 @@
 # This script will install iptables status check settings on a Debian 8.X server.
 
 # Download iptables config file for Zabbix agent
-wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Configs/iptables.conf -O /etc/zabbix/zabbix_agentd.d/iptables.conf
+wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Configs/iptables.conf -O /etc/zabbix/zabbix_agentd.conf.d/iptables.conf
 
 # Download iptablescheck shell script file for Zabbix agent
 wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Scripts/iptablescheck.sh -O /usr/local/bin/iptablescheck.sh
