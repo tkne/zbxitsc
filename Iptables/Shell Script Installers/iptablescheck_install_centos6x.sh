2215b5d772010abfa0a46263afca4b3874d479d1
tomislav <tomislav.knezevic@itsherpa.com>
2018-09-07 12:50:30 +0900

    Corrected error in link.

--- Iptables/Shell Script Installers/iptablescheck_install_centos6x.sh
+++ Iptables/Shell Script Installers/iptablescheck_install_centos6x.sh
@@ -5,7 +5,7 @@
 wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Configs/iptables.conf -O /etc/zabbix/zabbix_agentd.d/iptables.conf
 
 # Download iptablescheck shell script file for Zabbix agent
-wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Scriptsiptablescheck.sh -O /usr/local/bin/iptablescheck.sh
+wget https://raw.githubusercontent.com/tkne/zbxitsc/master/Iptables/Script/iptablescheck.sh -O /usr/local/bin/iptablescheck.sh
 
 # Make iptablescheck shell script file executable
 chmod +x /usr/local/bin/iptablescheck.sh
